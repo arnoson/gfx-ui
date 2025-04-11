f2ec03332c8bc95100e190fffffeb2f0c77d54b5
arnoson <hallo@arno-schlipf.info>
2025-04-11 13:41:53 +0200

    chore: cleanup

--- src/stores/history.ts
+++ src/stores/history.ts
@@ -17,17 +17,8 @@ const clone = <T>(value: T): T => structuredClone(toRaw(value))
 
 const frameToState = (frame: Frame) => {
   // Discard the scale, as we don't want it to change on undo/redo
-
-  try {
-    clone(toRaw(frame))
-  } catch (e) {
-    console.warn('oh no!', toRaw(frame))
-    console.error(e)
-  }
-
-  // const { scale, ...state } = clone(frame)
-  // return state
-  return {}
+  const { scale, ...state } = clone(frame)
+  return state
 }
 
 export const useHistory = defineStore('history', () => {
